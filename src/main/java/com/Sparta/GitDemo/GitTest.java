3d0ee459771e5e9174cfa231f13e2747eefb78e1
Usama Navid <usamanavid098@gmail.com>
2023-09-26 18:21:53 +0100

    if you are reading this

--- src/main/java/com/Sparta/GitDemo/GitTest.java
+++ src/main/java/com/Sparta/GitDemo/GitTest.java
@@ -4,4 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("This is my first project sending to Git");
     }
+
+	public static void test() {
+		int a = 5;
+	}
 }
